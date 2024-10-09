e66bae3314e2de9bc663941529d531e6cc538a48
SaeedAhmed56 <saeedahmed1908@gmail.com>
2024-10-09 18:04:03 +0530

    add var and datatype files

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,20 @@
+const accountId = 11233434
+let accountEmail = "SaeedAhmed@gmail.com"
+var accountPassword = "122254536546"
+accountCity = "Solapur"
+
+const accountState=56;
+
+// accountId =56 //not allowed
+
+accountEmail="SaAh@gmail.com"
+accountPassword= "123456789"
+accountCity="Dubai"
+
+/*
+Prefer Not to use Var
+Because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
