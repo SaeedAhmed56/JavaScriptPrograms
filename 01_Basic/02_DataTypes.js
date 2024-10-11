2cbbd89e8fc3498e795f984b77ec99a3f75c1205
SaeedAhmed56 <saeedahmed1908@gmail.com>
2024-10-12 00:28:36 +0530

    add conversionoperation of datatypes

--- 01_Basic/02_DataTypes.js
+++ 01_Basic/02_DataTypes.js
@@ -7,3 +7,16 @@ console.log("Saeed")
 let name = "Ahmed"
 let age = 21
 let isLoggedIn=false
+
+//number => 2 to power 52
+//bigint 
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object=
+
+console.log(typeof null)        //object
+console.log(typeof undefined)   //undefined
---
+++ 01_Basic/03_ConversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Saeed"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//  "33" => 33
+//  "33abc" => NaN      NotANumber
+//  true    => 1; false => 0
+
+let isLoggedIn = "Saeed"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false 
+//"Saeed" => true
+
+let someNumber=56
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
