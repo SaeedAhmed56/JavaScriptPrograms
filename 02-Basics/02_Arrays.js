ebe28a53f45cd6b35d1bb29a29322aea3a468bef
SaeedAhmed56 <saeedahmed1908@gmail.com>
2024-10-21 23:33:05 +0530

    add Object file

--- 02-Basics/02_Arrays.js
+++ 02-Basics/02_Arrays.js
@@ -1,2 +1,28 @@
-const marvel_heroes=["Thor","IronMan","SpiderMan"]
-const Dc=["Superman","BatMan","Flash"]
+const marvel_heros=["Thor","IronMan","SpiderMan"]
+const dc_heros=["Superman","BatMan","Flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[3]);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_new_Heros= [...marvel_heros,...dc_heros]
+// console.log(all_new_Heros);
+
+const another_array= [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array= another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Saeed"));
+console.log(Array.from("Saeed"));
+console.log(Array.from({name : "Saeed"}));      //interesting
+
+let score1 = 100
+let score2 = 200
+let score3= 300
+
+console.log(Array.of(score1,score2,score3));
---
+++ 02-Basics/03_Objects.js
@@ -0,0 +1,43 @@
+//singleton
+//object.create
+
+const { object } = require("yup");
+
+
+//object literals
+
+const mySym = Symbol("key1")
+
+const JsUser= {
+    name: "Saeed",
+    fullname : "Saeed Ahmed",
+    [mySym] : "mykey1",
+    age : 21,
+    location : "Maharashtra",
+    email : "saeed@google.com",
+    isLoggedIn : false,
+    LastLogingDays : ["Monday","Saturday"]
+}
+
+// console.log(JsUser.name);
+// console.log(JsUser["email"]);
+// console.log(JsUser["fullname"]);
+// console.log(typeof JsUser[mySym]);
+
+JsUser.email= "saeed@chatgpt.com"
+// console.log(JsUser.email);
+// Object.freeze(JsUser)
+JsUser.name="shaikh"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+    
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User ${this.name}`);
+    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
