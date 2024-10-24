7e944a3e7e488d8c48d05228ca9770edb1ca3541
SaeedAhmed56 <saeedahmed1908@gmail.com>
2024-10-24 23:51:10 +0530

    add iteration folder

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+//while loop
+// while (condition) {
+    
+// }
+
+// let index =0
+// while (index <=10) {
+//     console.log(`Value of Index :${index}`);
+//     // index = index +2
+//     index +=2
+// }
+
+// let myArray =["Thor","IronMan","BatMan","Hulk"]
+
+// let arr =0
+// while(arr<myArray.length){
+//     console.log(`Value is: ${myArray[arr]}`);
+//     arr++;
+    
+// }
+
+// do {
+    
+// } while (condition);
+
+let score =11
+do {
+    console.log(`Score is : ${score}`);
+    score++;
+    
+} while (score <=10);
